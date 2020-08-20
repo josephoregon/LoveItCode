f8a2cb2031476219c3e7575b18015e18e571e0bc
josephoregon <josephoregon@icloud.com>
2020-08-20 15:30:03 -0500

    Create json_to_pandas_df.py

---
+++ json_to_pandas_df.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# json to dataframe
+
+with open('Assessor-Search-Results.json', 'r', encoding='utf-8') as \
+    json_file:
+    json_work = json.load(json_file)
+
+df = pd.json_normalize(json_work)
+
+df = pd.DataFrame([y for x in df['results'].values.tolist() for y in x])
+
+
