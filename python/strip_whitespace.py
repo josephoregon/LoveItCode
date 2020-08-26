c7efd729714b8626ecfc27595b8650ba7b6d6164
josephoregon <josephoregon@icloud.com>
2020-08-26 15:03:27 -0500

    Update strip_whitespace.py

--- python/strip_whitespace.py
+++ python/strip_whitespace.py
@@ -3,3 +3,15 @@
 df_trimmed = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 
 df_col_trimmed = df.rename(columns=lambda x: x.strip())
+
+
+def panda_strip(x):
+    r =[]
+    for y in x:
+        if isinstance(y, str):
+            y = y.strip()
+
+        r.append(y)
+    return pd.Series(r)
+
+df = df.apply(lambda x: panda_strip(x))
