a4baf26faaf1a2caf04c39931e33e9c7e966e221
josephoregon <josephoregon@icloud.com>
2020-10-06 14:57:22 -0500

    Update reduce _df_memory.py

--- python/reduce _df_memory.py
+++ python/reduce _df_memory.py
@@ -9,7 +9,7 @@ def reduce_mem_usage(df):
     """
 
     start_mem = df.memory_usage().sum() / 1024 ** 2
-    print 'Memory usage of dataframe is {:.2f}MB'.format(start_mem)
+    print ('Memory usage of dataframe is {:.2f}MB'.format(start_mem))
 
     for col in df.columns:
         col_type = df[col].dtype
@@ -42,8 +42,8 @@ def reduce_mem_usage(df):
         else:
             df[col] = df[col].astype('category')
     end_mem = df.memory_usage().sum() / 1024 ** 2
-    print 'Memory usage after optimization is: {:.2f}MB'.format(end_mem)
-    print 'Decreased by {:.1f}%'.format(100 * (start_mem - end_mem)
+    print ('Memory usage after optimization is: {:.2f}MB'.format(end_mem))
+    print ('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem))
             / start_mem)
 
     return df
