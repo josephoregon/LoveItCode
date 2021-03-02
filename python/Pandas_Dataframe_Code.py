2358894553e65d2d6afca3e16704f5d2d417dc9f
josephoregon <josephoregon@icloud.com>
2021-03-02 16:27:02 -0600

    Update and rename pandas_df_code.py to Pandas_Dataframe_Code.py

--- python/Pandas_Dataframe_Code.py
+++ python/Pandas_Dataframe_Code.py
@@ -1,5 +1,8 @@
 # Create unique ID for rows
 df['id'] = df.groupby(['y','x']).ngroup()
 
+# Add leading zeros to the integer column in Python
+df['id']=df['id'].apply(lambda x: '{0:0>5}'.format(x))
+
 # Search substrings for any string value in dataframe
 df.apply(lambda row: row.astype(str).str.contains('STRING').any(), axis=1)
