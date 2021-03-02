71fb2de3c236a9de208f10be051a1bbc923c76ba
josephoregon <josephoregon@icloud.com>
2021-03-02 16:22:56 -0600

    add "Create unique ID for rows" code

--- python/pandas_df_code.py
+++ python/pandas_df_code.py
@@ -1,1 +1,5 @@
-DATAFRAME.apply(lambda row: row.astype(str).str.contains('STRING').any(), axis=1)
+# Create unique ID for rows
+df['id'] = df.groupby(['y','x']).ngroup()
+
+# Search substrings for any string value in dataframe
+df.apply(lambda row: row.astype(str).str.contains('STRING').any(), axis=1)
