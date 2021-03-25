a4046f88150afe9be146a9a1d7cf439de2816209
josephoregon <josephoregon@icloud.com>
2021-03-25 17:00:03 -0500

    create function notation formatting

---
+++ Python-Tools/function_notation.py
@@ -0,0 +1,18 @@
+"""
+------------
+DESCRIPTION:
+------------
+Write_brief_summary_of_what_the_function_will_do
+
+-----------
+PARAMETERS:
+-----------
+Parameter_1: Description_with_example_if_needed
+Parameter_2: Description_with_example_if_needed
+
+--------
+RETURNS:
+--------
+DataType: Describe_what_is_being_returned
+
+"""
