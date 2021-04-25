cf07cb35ed88a9bbc6f67346a718dde094ff2a1a
josephoregon <josephoregon@icloud.com>
2021-04-24 20:45:26 -0500

    Update data_cleanse.py

--- python/pandas/data_cleanse.py
+++ python/pandas/data_cleanse.py
@@ -1,5 +1,10 @@
 # Only drop columns which have at least 90% non-NaNs
 df.dropna(thresh=int(tube.shape[0] * 1), axis=1)
 
-# Filter dataframe to view NaNs
-def nans(df): return df[df.isnull().any(axis=1)]
+# Filter dataframe to VIEW NaNs
+def view_nans(df):
+    return df[df.isnull().any(axis=1)]
+
+# Filter dataframe to REMOVE NaNs
+def remove_nans(df):
+    return df[~(df.isnull().any(axis=1))].reset_index(drop=True)
